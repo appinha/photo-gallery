eb5f17387a73514ad1776a5e5000d5e16a1b461d
appinha <amanda_pinha@hotmail.com>
2024-04-30 14:35:49 -0300

    [server] Sets up database

--- server/README.md
+++ server/README.md
@@ -6,6 +6,23 @@
 make install
 ```
 
+2. Create database:
+
+- Open Docker
+- Run the command below (changing `user` and `password` values to your own):
+
+```Bash
+docker run --name photo_gallery-db -e POSTGRES_USER=user -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres
+```
+
+- Add the needed variables in the `.env` file, for example:
+
+```
+DB_USER=user
+DB_PASSWORD=password
+DB_HOST="localhost:5432/postgres"
+```
+
 2. Run api:
 
 ```Bash
--- server/app.py
+++ server/app.py
@@ -1,7 +1,19 @@
 import os
 from flask import Flask
 from flask_cors import CORS
+from api.database import db
 from api.views import views
+
+
+def setup_database(app):
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    db_host = os.getenv("DB_HOST")
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_user}:{db_password}@{db_host}"
+
+    with app.app_context():
+        db.init_app(app)
 
 
 def create_app():
@@ -9,6 +21,8 @@ def create_app():
     CORS(app)
     app.config.from_pyfile('settings.py')
     app.secret_key = os.getenv("APP_SECRET_KEY")
+
+    setup_database(app)
 
     for view in views:
         app.register_blueprint(view, url_prefix="/api")
