7df3676f9a7218d36ee0307feb5feb97fc764f96
appinha <amanda_pinha@hotmail.com>
2024-04-30 14:19:24 -0300

    [server] Creates endpoint to create and remove photo stars

--- server/api/photos/controllers.py
+++ server/api/photos/controllers.py
@@ -7,3 +7,11 @@ def get_all_photos(user):
         f.close()
 
     return [{**photo, 'is_starred': False} for photo in photos]
+
+
+def create_photo_like():
+    pass
+
+
+def delete_photo_like():
+    pass
--- server/api/photos/views.py
+++ server/api/photos/views.py
@@ -1,7 +1,7 @@
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 from flask.blueprints import Blueprint
 from api.utils.decorators import token_required
-from api.photos.controllers import get_all_photos
+from api.photos.controllers import get_all_photos, create_photo_like, delete_photo_like
 
 
 photos = Blueprint('photos', __name__)
@@ -15,3 +15,33 @@ def get_photos(user):
     except Exception:
         return make_response({"message": "Internal error"}, 500)
 
+
+@photos.post("/photos/star")
+@token_required
+def add_photo_like(user):
+    data = request.json
+    photo_id = data.get("id")
+
+    if not (photo_id):
+        return make_response({"message": "Photo id not provided"}, 400)
+
+    try:
+        create_photo_like()
+        return make_response({"message": "Photo like created"}, 201)
+    except Exception:
+        return make_response({"message": "Internal error"}, 500)
+
+
+@photos.delete("/photos/star")
+@token_required
+def remove_photo_like(user):
+    data = request.json
+    photo_id = data.get("id")
+
+    if not (photo_id):
+        return make_response({"message": "Photo id not provided"}, 400)
+    try:
+        delete_photo_like()
+        return make_response({"message": "Photo like deleted"}, 200)
+    except Exception:
+        return make_response({"message": "Internal error"}, 500)
